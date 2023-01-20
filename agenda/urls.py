88bb0c9eeb001cb98c844ee53dabc517288ffeb2
Pedro Neto <pedrofcn01@hotmail.com>
2023-01-20 14:59:34 -0300

    Adicionado funções de alteração e exclusão de dados

--- agenda/urls.py
+++ agenda/urls.py
@@ -20,10 +20,11 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/agenda')),
     path('agenda/', views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
-    path('', RedirectView.as_view(url='/agenda')),
+    path('agenda/evento/delete/<int:id_evento>', views.delete_evento),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user)
